3d9557b5cd281b0150e317d3f9c0b61a176d9e9a
Bartosz <grypsy@gmail.com>
2017-07-19 09:28:06 +0000

    year added

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /* 
- * Copyright (c) 2016, Bartosz Jakoktochce <grypsy@gmail.com>
+ * Copyright (c) 2016, 2017 Bartosz Jakoktochce <grypsy@gmail.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
